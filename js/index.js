77d568f5f65a72faf9311f4f0b47530b81b13f89
Eli <elize.san10@gmail.com>
2021-01-12 13:54:27 -0600

    have 5 event listeners applied to 5 elements.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,30 @@
 // Your code goes here
+
+const introHeader = document.querySelector('.intro')
+const busImage = introHeader.querySelector('img')
+
+//string is what event we want to use 
+busImage.addEventListener('mouseenter',() => {
+    busImage.style.transform = 'scale(1.2)'
+    busImage.style.transition = 'transform 1s'
+})
+ //make it so the bus image goes back to original size
+ //fade it back over 1s
+ busImage.addEventListener('mouseleave', () => {
+     busImage.style.transform = 'scale(1)'
+     busImage.style.trasition = 'transform 1s'
+ })
+
+//when you want to add an event listener to multiple events
+//use the for each method. e.g.
+document.querySelectorAll('.btn').forEach(title => {
+    title.addEventListener('click', (e) => {
+        console.log(e)
+        title.style.backgroundColor = 'red'
+    })
+}) 
+
+//whatever you put in the parameter is always going to 
+//be the event 
+
+//try to make your code as readable as possible 
